4c5e77f96c258b2131579f343ba1623452560bef
Sau Juliana <julienyange@gmail.com>
2022-07-08 01:00:06 +0300

    added starter code

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <linux/limits.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char* argv[], char* envp[])
+{
+
+	if (argc != 3) return fprintf(stderr, "Invalid arguments count! Should be 2 arguments!\n");
+
+	return (0);
+}
