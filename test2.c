ea192f2669836aff339b73e1cd6d5d115d4ae616
Sau Juliana <julienyange@gmail.com>
2022-07-13 17:21:06 +0300

    new changes

--- test2.c
+++ test2.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#include <fnmatch.h>
 
 static void lookup(char *rootPath, const char *filename)
 {
@@ -22,7 +23,7 @@ static void lookup(char *rootPath, const char *filename)
             if (fnmatch(filename, entity->d_name, 0) != 0)
             // if (strcmp(entity->d_name, filename) != 0)
             {
-                printf("%hhd %s/%s\n", entity->d_type, dirname, entity->d_name);
+                printf("%hhd %s/%s\n", entity->d_type, filename, entity->d_name);
                 continue;
             }
             // if there is a directory in the results of search, pritn out
@@ -32,11 +33,11 @@ static void lookup(char *rootPath, const char *filename)
                 strcat(path, filename);
                 strcat(path, "/");
                 strcat(path, entity->d_name);
-                lookup(path);
+                lookup(path, filename);
             }
-            entity = readdir(dir);
+            entity = readdir(dirp);
 
-            (void)printf("found %s\n", arg);
+            (void)printf("found %s\n", filename);
             (void)closedir(dirp);
             return;
         }
@@ -73,8 +74,13 @@ static void lookup(char *rootPath, const char *filename)
 
 int main(int argc, char *argv[])
 {
-    int i;
-    for (i = 1; i < argc; i++)
-        lookup(argv[i]);
+    if (argc != 3)
+        return fprintf(stderr, "Invalid arguments count! Should be 2 arguments!\n");
+
+    // printing the path to the directory we are searching in
+    char path[PATH_MAX];
+    sprintf(path, "%s", argv[2]);
+
+    lookup(path, &noOfFilesWatched, argv[1]);
     return (0);
 }
