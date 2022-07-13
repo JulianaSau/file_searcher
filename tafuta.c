f7ada4171120c592e328eeb4988d9284a7b7b12e
Sau Juliana <julienyange@gmail.com>
2022-07-13 23:20:06 +0300

    new changes

--- tafuta.c
+++ tafuta.c
@@ -68,7 +68,7 @@ int file_searcher(char *rootPath, char *fileName, char *searchParam)
             }
             else if (fnmatch(fileName, entity->d_name, 0) == 0)
             {
-                if (searchParam != " ")
+                if (searchParam != NULL)
                 {
                     search_file(fullPath, searchParam);
                     continue;
